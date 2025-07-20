98d33e6f94c7b4be1d66651d30c953ad845a8e11
DESKTOP-JJ7EVR4\Igor <ig-gri@mail.ru>
2025-07-20 13:08:16 +0300

    0.1.0-alpha: Fixed Camera can move outside map

--- rts-sandbox-src/Assets/Scripts/CameraController.cs
+++ rts-sandbox-src/Assets/Scripts/CameraController.cs
@@ -4,12 +4,17 @@ public class CameraController : MonoBehaviour
 {
     public Camera ControlledCamera;
 
+    private MapValues _mapValues;
+
     void Start()
     {
         if (ControlledCamera == null)
         {
             ControlledCamera = Camera.main;
         }
+
+        _mapValues = GameObject.FindGameObjectWithTag("GameController")
+            .GetComponent<MapValues>();
     }
 
     public void Set(Vector3 vector)
@@ -20,6 +25,20 @@ public class CameraController : MonoBehaviour
 
     public void Move(Vector3 vector)
     {
-        ControlledCamera.transform.position += vector;
+        var newPosition = ControlledCamera.transform.position + vector;
+
+        newPosition.x = Mathf.Clamp(
+            newPosition.x,
+            _mapValues.LeftTopMapCornerPosition.x,
+            _mapValues.RightBottomMapCornerPosition.x
+        );
+
+        newPosition.z = Mathf.Clamp(
+            newPosition.z,
+            _mapValues.RightBottomMapCornerPosition.z,
+            _mapValues.LeftTopMapCornerPosition.z
+        );
+
+        ControlledCamera.transform.position = newPosition;
     }
 }
